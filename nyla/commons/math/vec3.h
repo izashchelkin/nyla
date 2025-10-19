cef28892262c531e255b0ba391db7ee66047465a
izashchelkin <ihor.zashchelkin@osapiens.com>
2025-10-19 18:00:08 +0200

    math

--- nyla/commons/math/vec3.h
+++ nyla/commons/math/vec3.h
@@ -10,7 +10,13 @@ struct Vec3 {
   float z;
 };
 
-inline Vec3 operator-(const Vec3& v) { return {-v.x, -v.y, -v.z}; }
+inline Vec3 operator-(const Vec3& v) {
+  return {
+      -v.x,
+      -v.y,
+      -v.z,
+  };
+}
 
 inline void operator+=(Vec3& lhs, const Vec3& rhs) {
   lhs.x += rhs.x;
@@ -19,7 +25,11 @@ inline void operator+=(Vec3& lhs, const Vec3& rhs) {
 }
 
 inline Vec3 operator+(const Vec3& lhs, const Vec3& rhs) {
-  return {lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z};
+  return {
+      lhs.x + rhs.x,
+      lhs.y + rhs.y,
+      lhs.z + rhs.z,
+  };
 }
 
 inline void operator-=(Vec3& lhs, const Vec3& rhs) {
@@ -29,7 +39,11 @@ inline void operator-=(Vec3& lhs, const Vec3& rhs) {
 }
 
 inline Vec3 operator-(const Vec3& lhs, const Vec3& rhs) {
-  return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};
+  return {
+      lhs.x - rhs.x,
+      lhs.y - rhs.y,
+      lhs.z - rhs.z,
+  };
 }
 
 inline void operator*=(Vec3& lhs, float scalar) {
@@ -39,7 +53,11 @@ inline void operator*=(Vec3& lhs, float scalar) {
 }
 
 inline Vec3 operator*(const Vec3& lhs, float scalar) {
-  return {lhs.x * scalar, lhs.y * scalar, lhs.z * scalar};
+  return {
+      lhs.x * scalar,
+      lhs.y * scalar,
+      lhs.z * scalar,
+  };
 }
 
 inline Vec3 operator*(const float scalar, Vec3& rhs) { return rhs * scalar; }
@@ -51,13 +69,18 @@ inline void operator/=(Vec3& lhs, float scalar) {
 }
 
 inline Vec3 operator/(const Vec3& lhs, float scalar) {
-  return {lhs.x / scalar, lhs.y / scalar, lhs.z / scalar};
+  return {
+      lhs.x / scalar,
+      lhs.y / scalar,
+      lhs.z / scalar,
+  };
 }
 
 //
 
 inline float Len(const Vec3& v) {
-  return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+  const float sqsum = v.x * v.x + v.y * v.y + v.z * v.z;
+  return std::sqrt(sqsum);
 }
 
 inline Vec3 Normalize(const Vec3& v) {
@@ -65,13 +88,24 @@ inline Vec3 Normalize(const Vec3& v) {
   return len ? v / len : v;
 }
 
+inline Vec3 Scale(const Vec3& lhs, const Vec3& rhs) {
+  return {
+      lhs.x * rhs.x,
+      lhs.y * rhs.y,
+      lhs.z * rhs.z,
+  };
+}
+
 inline float Dot(const Vec3& lhs, const Vec3& rhs) {
   return lhs.x + rhs.x + lhs.y * rhs.y + lhs.z + rhs.z;
 }
 
 inline Vec3 Cross(const Vec3& lhs, const Vec3& rhs) {
-  return {lhs.y * rhs.z - lhs.z * rhs.y, lhs.z * rhs.x - lhs.x * rhs.z,
-          lhs.x * rhs.y - lhs.y * rhs.x};
+  return {
+      lhs.y * rhs.z - lhs.z * rhs.y,
+      lhs.z * rhs.x - lhs.x * rhs.z,
+      lhs.x * rhs.y - lhs.y * rhs.x,
+  };
 }
 
 }  // namespace nyla
