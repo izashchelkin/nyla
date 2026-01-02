2c1506a1ca47ebd0cd667a2be28379617a569496
izashchelkin <ihor.zashchelkin@osapiens.com>
2026-01-02 18:46:24 +0100

    a!

--- scripts/build_ctags.ps1
+++ scripts/build_ctags.ps1
@@ -1,2 +1,1 @@
-REM ctags -R --languages=C,C++ --extras=+q --fields=+iaS 
 ctags -R --languages=C,C++ --langmap=C++:+.h.hpp.h++.hh.hxx --kinds-C++=+p --extras=+q --fields=+iaS -f tags nyla third_party
