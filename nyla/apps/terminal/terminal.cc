a57af4a8be1b59ad22ab80f9468bef7d2024ba24
Ihor Zashchelkin <ihorz@tuta.com>
2026-02-27 03:13:24 +0100

    A

---
+++ nyla/apps/terminal/terminal.cc
@@ -0,0 +1,37 @@
+#include "nyla/platform/platform.h"
+#include "nyla/rhi/rhi.h"
+
+namespace nyla
+{
+
+auto PlatformMain() -> int
+{
+    g_Platform.Init(PlatformInitDesc{
+        .enabledFeatures = PlatformFeature::KeyboardInput,
+        .open = true,
+    });
+
+    g_Rhi.Init(RhiInitDesc{
+        .flags = RhiFlags::VSync,
+        .limits =
+            {
+                .numTextures = 0,
+                .numTextureViews = 0,
+                .numBuffers = 0,
+                .numSamplers = 0,
+
+                .numFramesInFlight = 1,
+                .maxDrawCount = 1,
+                .maxPassCount = 1,
+
+                .frameConstantSize = 0,
+                .passConstantSize = 0,
+                .drawConstantSize = 0,
+                .largeDrawConstantSize = 320,
+            },
+    });
+
+    return 0;
+}
+
+} // namespace nyla
