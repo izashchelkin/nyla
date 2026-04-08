96605f3fff271a92d9083f0a6198a42481f2e96a
Ihor Zashchelkin <me@ihorzash.com>
2026-04-08 19:16:37 +0200

    script

---
+++ git-am-nvim.ps1
@@ -0,0 +1,13 @@
+$tmp = [System.IO.Path]::GetTempFileName()
+try {
+    nvim $tmp
+    
+    if ((Test-Path $tmp) -and (Get-Item $tmp).Length -gt 0) {
+        git am $tmp
+    } else {
+        Write-Warning "Datei leer. git am wurde nicht ausgeführt."
+    }
+} 
+finally {
+    if (Test-Path $tmp) { Remove-Item $tmp }
+}
